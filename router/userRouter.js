6623fd1aa0d5cfe225387836d70d2b9e5fbac183
Моломжамц Цогтбаатар <HP230389@G303-CC0155.local>
2024-02-20 19:21:13 +0800

    fixed back c1

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,38 +1,37 @@
-const {Router} = require('express')
-const bcrypt = require('bcrypt')
-const { createUser, loginuser} = require('../controller/userController');
-const userModel = require('../database/Schema/userSchema')
-
+const { Router } = require("express");
+const bcrypt = require("bcrypt");
+const { createUser, loginuser } = require("../controller/userController");
+const userModel = require("../database/Schema/userSchema");
 
 const userRouter = Router();
 const validateEmailAddress = async (req, res, next) => {
-    const body = req.body;
-    const user = await userModel.findOne({ email: body.email });
-    if (user) {
-      res.status(403).send("email has already taken");
-    } else {
-      next();
-    }
-  };
+  const body = req.body;
+  const user = await userModel.findOne({ email: body.email });
+  if (user) {
+    res.status(403).send("email has already taken");
+  } else {
+    next();
+  }
+};
 const validatePassword = async (req, res, next) => {
-    const body = req.body;
-    const user = await userModel.findOne({ email: body.email });
-    console.log(body)
-    console.log(user)
-    if (user) {
-      const isPasswordCorrect = bcrypt.compareSync(body.password, user.password);
-      if (isPasswordCorrect) {
-          next();
-        } else {
-            console.log(isPasswordCorrect)
-        res.status(404).send("incorrect password");
-      }
+  const body = req.body;
+  const user = await userModel.findOne({ email: body.email });
+  // const signup = user.password + user.email;
+  // console.log(user.password, user.email);
+  if (user) {
+    const isPasswordCorrect = bcrypt.compareSync(body.password, user.password);
+    if (isPasswordCorrect) {
+      next();
     } else {
-      res.status(404).send("user not found");
+      console.log(isPasswordCorrect);
+      res.status(404).send("incorrect password");
     }
-  };
+  } else {
+    res.status(404).send("user not found");
+  }
+};
 
-  userRouter.post('/login',validatePassword, loginuser)
-  userRouter.post('/signup',validateEmailAddress, createUser)
+userRouter.post("/login", validatePassword, loginuser);
+userRouter.post("/signup", validateEmailAddress, createUser);
 
-module.exports = userRouter
+module.exports = userRouter;
